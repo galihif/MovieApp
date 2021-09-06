baaedfd00f622c8844ea1cda2f99114f735637a6
galihif <galihindra2401@mail.ugm.ac.id>
2021-09-06 08:27:04 +0700

    add DetailActivity.kt

---
+++ app/src/main/java/com/giftech/movieapp/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.giftech.movieapp.ui.detail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.giftech.movieapp.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
