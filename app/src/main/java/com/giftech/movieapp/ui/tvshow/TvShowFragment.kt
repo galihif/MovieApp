2fecf47c40c8c6b26455c2a46b632b83a136841e
galihif <galihindra2401@mail.ugm.ac.id>
2021-09-07 10:17:47 +0700

    fix warning in TvShowFragment.kt

--- app/src/main/java/com/giftech/movieapp/ui/tvshow/TvShowFragment.kt
+++ app/src/main/java/com/giftech/movieapp/ui/tvshow/TvShowFragment.kt
@@ -16,7 +16,7 @@ class TvShowFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentTvShowBinding.inflate(layoutInflater)
         return binding.root
     }
