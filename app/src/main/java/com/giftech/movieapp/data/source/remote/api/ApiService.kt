3d37c9a72e14f8bc5d3a9ddd48515bbe393734e6
galihif <galihindra2401@mail.ugm.ac.id>
2021-09-09 21:39:05 +0700

    add get tv by id method endpoint

--- app/src/main/java/com/giftech/movieapp/data/source/remote/api/ApiService.kt
+++ app/src/main/java/com/giftech/movieapp/data/source/remote/api/ApiService.kt
@@ -3,6 +3,7 @@ package com.giftech.movieapp.data.source.remote.api
 import com.giftech.movieapp.data.source.remote.MovieResponse
 import com.giftech.movieapp.data.source.remote.api.ApiConfig.Companion.API_KEY
 import com.giftech.movieapp.data.source.remote.response.DetailMovieResponse
+import com.giftech.movieapp.data.source.remote.response.DetailTvResponse
 import com.giftech.movieapp.data.source.remote.response.TvResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -17,5 +18,8 @@ interface ApiService {
 
     @GET("/3/tv/popular?api_key=$API_KEY")
     fun getTvs(): retrofit2.Call<TvResponse>
+
+    @GET("/3/tv/{id}?api_key=$API_KEY")
+    fun getTvById(@Path("id") id:Int):retrofit2.Call<DetailTvResponse>
 
 }
