ff4ce41e9f60f1212ffebe2034dfba5767a5c1ff
galihif <galihindra2401@mail.ugm.ac.id>
2021-09-10 09:00:42 +0700

    add unit test in DetailViewModelTest.kt

--- app/src/test/java/com/giftech/movieapp/ui/detail/DetailViewModelTest.kt
+++ app/src/test/java/com/giftech/movieapp/ui/detail/DetailViewModelTest.kt
@@ -1,24 +1,41 @@
 package com.giftech.movieapp.ui.detail
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.giftech.movieapp.data.FilmEntity
 import com.giftech.movieapp.data.source.FilmRepository
+import com.giftech.movieapp.utils.MovieDummy
+import com.giftech.movieapp.utils.TvShowDummy
+import com.nhaarman.mockitokotlin2.verify
 import junit.framework.Assert.assertEquals
 import junit.framework.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class DetailViewModelTest {
 
     private lateinit var viewModel: DetailViewModel
+    private val dummyMovie = MovieDummy.generateDummyMovies()[0]
+    private val dummyTv = TvShowDummy.generateDummyTvShows()[0]
+
+    private val movieId = dummyMovie.id
+    private val tvId = dummyTv.id
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
     @Mock
     private lateinit var filmRepository: FilmRepository
+
+    @Mock
+    private lateinit var filmObserver:Observer<FilmEntity>
 
     @Before
     fun setUp() {
@@ -27,32 +44,39 @@ class DetailViewModelTest {
 
     @Test
     fun getMovieById() {
-        val movie = FilmEntity(
-            1,
-            "Iron Man",
-            "Superhero, Action",
-            "When Tony Stark, an industrialist," +
-                    " is captured, he constructs a high-tech armoured suit to escape. " +
-                    "Once he manages to escape, he decides to use his suit to fight " +
-                    "against evil forces to save the world.",
-            "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSMoliPRgy5xgoyg_eZmuKyptS9s1weVooX5tLqTBnGBYMRtE-t"
-        )
-        val mMovie = viewModel.getMovieById(1)
-        assertNotNull(mMovie)
-        assertEquals(movie,mMovie)
+        val movie = MutableLiveData<FilmEntity>()
+        movie.value = dummyMovie
+
+        `when`(filmRepository.getMoviesById(movieId!!)).thenReturn(movie)
+        val movieEntity = viewModel.getMovieById(movieId).value as FilmEntity
+
+        assertNotNull(movieEntity)
+        assertEquals(dummyMovie.id, movieEntity.id)
+        assertEquals(dummyMovie.title, movieEntity.title)
+        assertEquals(dummyMovie.genre, movieEntity.genre)
+        assertEquals(dummyMovie.sinopsis, movieEntity.sinopsis)
+        assertEquals(dummyMovie.poster, movieEntity.poster)
+
+        viewModel.getMovieById(movieId).observeForever(filmObserver)
+        verify(filmObserver).onChanged(dummyMovie)
     }
 
     @Test
     fun getTvById() {
-        val tvShow = FilmEntity(
-            1,
-            "WandaVision",
-            "Superhero, Action",
-            "Living idealized suburban lives, super-powered beings Wanda and Vision begin to suspect that everything is not as it seems.",
-            "https://m.media-amazon.com/images/M/MV5BZGEwYmMwZmMtMTQ3MS00YWNhLWEwMmQtZTU5YTIwZmJjZGQ0XkEyXkFqcGdeQXVyMTI5MzA5MjA1._V1_.jpg"
-        )
-        val mTvShow = viewModel.getTvById(1)
-        assertNotNull(mTvShow)
-        assertEquals(tvShow,mTvShow)
+        val tv = MutableLiveData<FilmEntity>()
+        tv.value = dummyTv
+
+        `when`(filmRepository.getMoviesById(tvId!!)).thenReturn(tv)
+        val tvEntity = viewModel.getMovieById(tvId).value as FilmEntity
+
+        assertNotNull(tvEntity)
+        assertEquals(dummyTv.id, tvEntity.id)
+        assertEquals(dummyTv.title, tvEntity.title)
+        assertEquals(dummyTv.genre, tvEntity.genre)
+        assertEquals(dummyTv.sinopsis, tvEntity.sinopsis)
+        assertEquals(dummyTv.poster, tvEntity.poster)
+
+        viewModel.getMovieById(tvId).observeForever(filmObserver)
+        verify(filmObserver).onChanged(dummyTv)
     }
 }
