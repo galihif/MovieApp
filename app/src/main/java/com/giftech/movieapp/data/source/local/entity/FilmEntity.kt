9f46ba3496b7bde1aaa26dc307d5bf08a650b0ac
galihif <galihindra2401@mail.ugm.ac.id>
2021-09-12 15:45:38 +0700

    add dao

--- app/src/main/java/com/giftech/movieapp/data/source/local/entity/FilmEntity.kt
+++ app/src/main/java/com/giftech/movieapp/data/source/local/entity/FilmEntity.kt
@@ -1,19 +1,37 @@
 package com.giftech.movieapp.data.source.local.entity
 
 import android.os.Parcelable
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 @Entity(tableName = "filmentities")
 data class FilmEntity (
 
-
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "id")
     var id:Int?,
+
+    @ColumnInfo(name = "title")
     var title:String?,
+
+    @ColumnInfo(name = "genre")
     var genre:String?,
+
+    @ColumnInfo(name = "sinopsis")
     var sinopsis:String?,
+
+    @ColumnInfo(name = "poster")
     var poster:String?,
+
+    @ColumnInfo(name = "bookmarked")
     var bookmarked:Int?,
+
+    @ColumnInfo(name = "type")
     var type:Int? //0 for movie 1 for tv
+
     ): Parcelable
---
+++ app/src/main/java/com/giftech/movieapp/data/source/local/room/FilmDao.kt
@@ -0,0 +1,37 @@
+package com.giftech.movieapp.data.source.local.room
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.giftech.movieapp.data.source.local.entity.FilmEntity
+
+@Dao
+interface FilmDao {
+
+    @Query("SELECT * FROM filmentities WHERE type = 0")
+    fun getAllMovies():LiveData<List<FilmEntity>>
+
+    @Query("SELECT * FROM filmentities WHERE type = 1")
+    fun getAllTvs():LiveData<List<FilmEntity>>
+
+    @Query("SELECT * FROM filmentities WHERE type = 1 AND bookmarked = 1")
+    fun getBookmarkedMovies():LiveData<List<FilmEntity>>
+
+    @Query("SELECT * FROM filmentities WHERE type = 0 AND bookmarked = 1")
+    fun getBookmarkedTvs():LiveData<List<FilmEntity>>
+
+    @Transaction
+    @Query("SELECT * FROM filmentities WHERE type = 0 AND id = :id")
+    fun getMovieById(id:String):LiveData<FilmEntity>
+
+    @Transaction
+    @Query("SELECT * FROM filmentities WHERE type = 1 AND id = :id")
+    fun getTvById(id:String):LiveData<FilmEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertFilms(films:List<FilmEntity>)
+
+    @Update
+    fun updateFilm(film:FilmEntity)
+
+
+}
