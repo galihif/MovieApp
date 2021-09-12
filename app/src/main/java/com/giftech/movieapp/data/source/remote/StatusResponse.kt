d651e1795d135bc3af0a11f78e162be2015cc930
galihif <galihindra2401@mail.ugm.ac.id>
2021-09-12 16:06:52 +0700

    add ApiResponse.kt and StatusResponse.kt u

---
+++ app/src/main/java/com/giftech/movieapp/data/source/remote/StatusResponse.kt
@@ -0,0 +1,7 @@
+package com.giftech.movieapp.data.source.remote
+
+enum class StatusResponse {
+    SUCCESS,
+    EMPTY,
+    ERROR
+}
--- app/src/main/java/com/giftech/movieapp/vo/Status.kt
+++
@@ -1,7 +0,0 @@
-package com.giftech.movieapp.vo
-
-enum class Status {
-    SUCCESS,
-    ERROR,
-    LOADING
-}
